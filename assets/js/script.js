62c1f3dd7a0fc5a7d7173e8989b5d2990503078c
imanmansour86 <imanmansour86@gmail.com>
2021-12-27 13:58:45 -0800

    adding timer code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-console.log("test");
+var quizTime = 5;
+
+var timer = document.querySelector(".time");
+var startQuizBtn = document.getElementById("start-quiz");
+var questionBody = document.getElementById("question-body");
+
+startQuizBtn.addEventListener("click", setTime);
+
+function setTime() {
+  var timeInterval = setInterval(function () {
+    questionBody.style.display = "block";
+    if (quizTime > 0) {
+      timer.textContent = "Time remaining: " + quizTime;
+      quizTime--;
+    }
+
+    if (quizTime === 0) {
+      console.log("stop here");
+      clearInterval(timeInterval);
+      timer.textContent = "Time is up! ";
+    }
+  }, 500);
+}
