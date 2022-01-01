ff4dbf22e9ec49b0d85bafcf80737cd4669a86d6
imanmansour86 <imanmansour86@gmail.com>
2021-12-31 23:40:22 -0800

    subtract time from clock when wrong answer is chosen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,25 @@
-var quizTime = 5;
+var quizTime = 100;
 var score = 0;
+let j = 0; //question index
 
 var timer = document.querySelector(".time");
 var startQuizBtn = document.getElementById("start-quiz");
+var nextQuestion = document.getElementById("next-question");
+var questionHeader = document.createElement("div");
+var highScore = document.getElementById("high-scores");
+
+nextQuestion.style.display = "none";
 var mainContainer = document.querySelector(".main-container");
 
 startQuizBtn.addEventListener("click", setTime);
 
 function setTime() {
+  mainContainer.style.display = "none";
+
+  answerQuestion();
   var timeInterval = setInterval(function () {
-    questionHeader.style.display = "block";
-    mainContainer.style.display = "none";
+    // questionHeader.style.display = "block";
+
     if (quizTime > 0) {
       timer.textContent = "Time remaining: " + quizTime;
       quizTime--;
@@ -20,6 +29,8 @@ function setTime() {
       console.log("time is up");
       clearInterval(timeInterval);
       timer.textContent = "Time is up! ";
+      nextQuestion.style.display = "none";
+      questionHeader.style.display = "none";
     }
   }, 500);
 }
@@ -58,7 +69,7 @@ var quizQuestions = [
     correctAnswer: "an object-oriented scripting language",
   },
   {
-    question: "What is mean by this keyword in javascript?",
+    question: "What is meant by this keyword in javascript?",
     answers: [
       "refers to current object",
       "refers to previous object",
@@ -69,75 +80,82 @@ var quizQuestions = [
   },
 ];
 
-var body = document.body;
+nextQuestion.addEventListener("click", function () {
+  j += 1;
 
-//bigger loop to loop over entire questions
+  if (j > 0) {
+    //remove the old question
+    document.getElementById(j - 1).remove();
+  }
+  if (j >= quizQuestions.length) {
+    console.log("test here" + quizQuestions.length);
+    alert("no more questions");
+  } else {
+    answerQuestion();
+  }
+});
 
-let j = 0;
+highScore.addEventListener("click", function () {
+  console.log("view high score");
+});
 
-var question = document.createElement("div");
+function answerQuestion() {
+  var body = document.body;
 
-var questionHeader = document.createElement("div");
-questionHeader.setAttribute("id", j);
-// questionHeader.style.display = "none";
+  var question = document.createElement("div");
 
-// var currentPosition = 0;
-// if {
+  questionHeader.setAttribute("id", j);
 
-// }
-//  (currentPosition < quizQuestions.length)
-//   var t = document.getElementById("j");
-//   t.style.display = "block";
+  questionHeader.textContent = quizQuestions[j].question;
+  questionHeader.classList.add("question-header");
+  question.appendChild(questionHeader);
+  body.appendChild(question);
 
-questionHeader.textContent = quizQuestions[j].question;
-questionHeader.classList.add("question-header");
-question.appendChild(questionHeader);
-body.appendChild(question);
+  var questionResult = document.createElement("div");
+  questionResult.classList.add("question-result");
 
-var questionResult = document.createElement("div");
+  //loop to create radio buttons for  question
+  for (i = 0; i < quizQuestions[j].answers.length; i++) {
+    var radioContainer = document.createElement("div");
 
-//loop to create radio buttons for  question
-for (i = 0; i < quizQuestions[j].answers.length; i++) {
-  var radioContainer = document.createElement("div");
+    questionHeader.appendChild(radioContainer);
 
-  questionHeader.appendChild(radioContainer);
+    radioContainer.classList.add("radio-sub");
 
-  radioContainer.classList.add("radio-sub");
+    var radioBtn = document.createElement("input");
+    radioBtn.setAttribute("type", "radio");
+    radioBtn.setAttribute("name", "q[" + j + "]");
+    radioBtn.setAttribute("value", quizQuestions[j].answers[i]);
+    radioBtn.setAttribute("id", "q[" + j + "]" + "-" + i);
+    radioContainer.appendChild(radioBtn);
 
-  var radioBtn = document.createElement("input");
-  radioBtn.setAttribute("type", "radio");
-  radioBtn.setAttribute("name", "q1");
-  radioBtn.setAttribute("value", quizQuestions[j].answers[i]);
-  radioBtn.setAttribute("id", "q[" + j + "]");
-  radioContainer.appendChild(radioBtn);
+    var radioLabel = document.createElement("label");
+    radioLabel.innerHTML = quizQuestions[j].answers[i];
+    radioContainer.appendChild(radioLabel);
 
-  var radioLabel = document.createElement("label");
-  radioLabel.innerHTML = quizQuestions[j].answers[i];
-  radioContainer.appendChild(radioLabel);
+    // const jCopy = j;
+    radioBtn.addEventListener("click", function (e) {
+      console.log("the click is ", e.target.value);
 
-  // take a copy of j so that the listener does not reference the final j when it is clicked
-  const jCopy = j;
-  radioBtn.addEventListener("click", function (e) {
-    console.log("the click is ", e.currentTarget.id);
+      document.getElementById(j).appendChild(questionResult);
+      console.log("jcopy is " + j);
+      var correctChoice = quizQuestions[j].correctAnswer;
 
-    var t = document.getElementById(jCopy);
-    console.log("test333+ " + t);
+      if (e.target.value == correctChoice) {
+        console.log("user answer is", e.target.value);
+        score += 1;
+        console.log("the answer is correct, score is " + score);
+        questionResult.textContent = "Correct answer";
+      } else {
+        score -= 1;
+        quizTime -= 10;
 
-    document.getElementById(jCopy).appendChild(questionResult);
+        console.log("the answer is wrong, score is ", score);
 
-    var correctChoice = quizQuestions[jCopy].correctAnswer;
+        questionResult.textContent = "Wrong answer";
+      }
+    });
+  }
 
-    if (e.target.value == correctChoice) {
-      console.log("user answer is", e.target.value);
-      score += 1;
-      console.log("the answer is correct, score is " + score);
-
-      questionResult.textContent = "the answer is correct";
-    } else {
-      score -= 1;
-      console.log("wrong answer, new scroe is ", score);
-
-      questionResult.textContent = "the answer is wrong";
-    }
-  });
+  nextQuestion.style.display = "block";
 }
